bc7017086061bdbbffcc0f41a4416f0dfc123ca1
Praveenvrao <vpraveenrao453@gmail.com>
2026-05-27 17:35:38 +0530

    Radiobutton Action

--- Tests/PWActions.spec.ts
+++ Tests/PWActions.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect, Locator} from "@playwright/test";
 
 // This test verifies the Playwright Actions
-test("Verify Playwright Actions", async ({page})=>{
+test.only("Verify Playwright Actions", async ({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/");
     const Name:Locator = page.locator("css=#name");
     await expect(Name).toBeVisible();
@@ -16,6 +16,13 @@ test("Verify Playwright Actions", async ({page})=>{
 
     await page.waitForTimeout(2000);
     await page.screenshot({path: 'test-results/screenshots/PWActions_Name.png'});
+
+    const Male_Gender:Locator = page.locator("css=#male");
+    await expect(Male_Gender).toBeVisible();
+    await expect(Male_Gender).toBeEnabled();
+    await Male_Gender.check();
+    await page.waitForTimeout(2000);
+    await page.screenshot({path: 'test-results/screenshots/PWActions_Radiobutton.png'});
 
     
     
